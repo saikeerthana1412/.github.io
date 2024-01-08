b17471c74b7750e13dfd590182c7b7980e658b01
Sai Keerthana <137150706+saikeerthana1412@users.noreply.github.com>
2024-01-08 18:32:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    strings:["Full Stack Developer", "Frontend Developer", "AI/ML Developer"],
+    typeSpeed: 85,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
